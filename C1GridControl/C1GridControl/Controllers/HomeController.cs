9fc18bee0e621bf6d9c498a4e67303da105d5e3d
Joel Parks <joel.patrick.parks@gmail.com>
2019-08-28 12:57:46 -0400

    Project Commit
    Full project commit to github for C1 .NET Core for Mac

---
+++ C1GridControl/C1GridControl/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using C1GridControl.Models;
+
+using System.Collections;
+using System.Globalization;
+using System.Web;
+using C1.Web.Mvc;
+
+namespace C1GridControl.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Sale.GetData(15));
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
